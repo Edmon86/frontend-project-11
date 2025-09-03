dfe4c74a7251de5f7228de40b0504cf9f5f4f3c5
Edmon <19ed86boy@mail.ru>
2025-09-03 18:29:43 +0300

    init progect

---
+++ eslint.config.cjs
@@ -0,0 +1,29 @@
+const js = require('@eslint/js')
+const globals = require('globals')
+const { defineConfig } = require('eslint/config')
+
+module.exports = defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js: js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      // Стили
+      'semi': ['error', 'never'],
+      'quotes': ['error', 'single'],
+      'indent': ['error', 2],
+      'comma-dangle': ['error', 'always-multiline'],
+      'space-before-function-paren': ['error', 'never'],
+      'object-curly-spacing': ['error', 'always'],
+      'space-infix-ops': 'error',
+      'space-before-blocks': ['error', 'always'],
+      'keyword-spacing': ['error', { before: true, after: true }],
+      'comma-spacing': ['error', { before: false, after: true }],
+      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+      'space-in-parens': ['error', 'never'],
+      'no-trailing-spaces': ['error'],
+      'semi-spacing': ['error', { before: false, after: true }],
+    },
+  },
+])
--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-const form = document.getElementById('rss-form');
-const input = document.getElementById('rss-url');
-const feedsContainer = document.getElementById('feeds');
+const form = document.getElementById('rss-form')
+const input = document.getElementById('rss-url')
+const feedsContainer = document.getElementById('feeds')
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const url = input.value;
+  e.preventDefault()
+  const url = input.value
   fetch(url)
     .then(response => {
       if (!response.ok) {
-        throw new Error('Ошибка сети');
+        throw new Error('Ошибка сети')
       }
-      return response.text();
+      return response.text()
     })
-    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then(str => new window.DOMParser().parseFromString(str, 'text/xml'))
     .then(data => {
-      const items = Array.from(data.querySelectorAll('item'));
-      feedsContainer.innerHTML = items.map(item => `<div class="mb-2"><h5>${item.querySelector('title').textContent}</h5><p>${item.querySelector('description').textContent}</p></div>`).join('');
+      const items = Array.from(data.querySelectorAll('item'))
+      feedsContainer.innerHTML = items.map(item => `<div class="mb-2"><h5>${item.querySelector('title').textContent}</h5><p>${item.querySelector('description').textContent}</p></div>`).join('')
     })
     .catch(err => {
-      console.error(err);
-      alert('Не удалось загрузить RSS');
-    });
-});
-
+      console.error(err)
+      alert('Не удалось загрузить RSS')
+    })
+})
